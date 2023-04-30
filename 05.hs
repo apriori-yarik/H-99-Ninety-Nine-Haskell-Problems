e773658be6921ad2e50b977d201bc5db22f97dca
Yaroslav Dokuzov <yaroslav.dokuzov@primeholding.com>
2023-04-30 23:55:05 +0300

    05 - fixed recursion

--- 05.hs
+++ 05.hs
@@ -2,9 +2,7 @@ myReverse :: [a] -> [a]
 myReverse = reverse
 
 -- does not work
-{-
 myReverse' :: [a] -> [a]
 myReverse' [] = []
 myReverse' [x] = [x]
-myReverse' (x:xs) = (myReverse' xs) ++ x
--}
+myReverse' (x:xs) = (myReverse' xs) ++ [x]
